4a43dc78ee6e749182bac42e16139af8ccd20c97
Victor Hugo Apnas <104700826+Victorapenas@users.noreply.github.com>
2023-07-19 23:55:51 -0300

    Update funcao.php

--- funcao.php
+++ funcao.php
@@ -1,22 +1,27 @@
 <?php
 function banco($dbHost, $dbUsername, $dbPassword, $dbName, $consulta)
 {
+    // Cria uma instância do objeto MySQLi para estabelecer a conexão com o banco de dados
     $banco = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    // Verifica se houve algum erro de conexão
     if ($banco->connect_error) {
         echo "Falha de conexão de referência: (" . $banco->connect_errno . ") - " . $banco->connect_error;
         echo "<a href='index.php'>Voltar</a>";
-        exit();
+        exit(); // Encerra a execução do script em caso de falha de conexão
     }
 
+    // Executa a consulta no banco de dados
     $resultado = $banco->query($consulta);
 
-    if ($resultado === true) {
-        $banco->close();
-        return true;
-    } else {
+    // Verifica se houve algum erro na consulta
+    if ($resultado === false) {
         echo "Falha na consulta referência: (" . $banco->errno . ") - " . $banco->error;
         echo "<a href='index.php'>Voltar</a>";
-        $banco->close();
-        return false;
+        $banco->close(); // Fecha a conexão com o banco de dados
+        return false; // Retorna falso para indicar que a consulta falhou
     }
+    
+    return $resultado; // Retorna o resultado da consulta bem-sucedida
 }
+?>
