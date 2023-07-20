bd7f538a4cbd98c6d17cec4c4b12248ba4751d7b
Victor Hugo Apnas <104700826+Victorapenas@users.noreply.github.com>
2023-07-19 23:53:24 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
+// Configurações do banco de dados
+$dbHost = 'localhost';      // O endereço do servidor do banco de dados
+$dbUsername = 'root';       // O nome de usuário para acessar o banco de dados
+$dbPassword = '';           // A senha do usuário para acessar o banco de dados
+$dbName = 'srscjf_if';      // O nome do banco de dados que será utilizado
 
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'srscjf_if';
-    
-
+// Cria uma instância do objeto MySQLi para estabelecer a conexão com o banco de dados
+$mysqli = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 ?>
